1a9227cdc2ea4de5ec9c2ad35d32f6bc92d06ec6
Nícolas <nicolassilva0109@gmail.com>
2024-02-23 11:08:06 -0300

    guh

--- source/apps/chil/check_ip_list.py
+++ source/apps/chil/check_ip_list.py
@@ -1,3 +1,7 @@
-from source.utils.manyprint.mprint import multi_print as printm
-from source.apps.chil.lib.ip_list import load_toml, add_entry
-from source.utils import console_messages
+import sys
+
+sys.path.append("../../../../source")
+from utils.manyprint.mprint import multi_print as printm
+from apps.chil.lib.ip_list import load_toml, add_entry
+from utils import console_messages
+
