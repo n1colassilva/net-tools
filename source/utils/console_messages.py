3fb827b9f5386a7668a0709d241cb95796ae50a5
Nícolas Sousa <nicolassilva0109@gmail.com>
2024-03-14 11:33:32 -0300

    added info tag

--- source/utils/console_messages.py
+++ source/utils/console_messages.py
@@ -6,11 +6,12 @@ from typing import Literal
 from termcolor import colored
 
 # This is to help intellisense
-Tag = Literal["success","hint", "warning", "error"]
+Tag = Literal["success", "hint", "info", "warning", "error"]
 
 TAGS: dict[Tag, str] = {
     "success": colored("SUCCESS:", "white", "on_green"),
     "hint": colored("HINT:", "white", "on_blue"),
+    "info": colored("INFO:", "white", "on_blue"),
     "warning": colored("WARNING:", "white", "on_yellow"),
     "error": colored("ERROR:", "white", "on_red"),
 }
