68ee2050f4dde2d9c8acc2804c283483323d4448
Nícolas Sousa <nicolassilva0109@gmail.com>
2024-03-15 11:04:39 -0300

    Added help function and easier versioning

--- source/user_interface.py
+++ source/user_interface.py
@@ -2,38 +2,43 @@ import getpass
 import os
 from typing import Optional
 from termcolor import colored as clr
+from utils.manyprint.mprint import multi_print as mprint
+
+VERSION_NUMBER = "0.0.3"
+VERSION_NAME = f"{clr('Alpha', 'green', attrs=['bold'])}"
 
 
 # Show splash screen
 def display_splash():
     """Prints out the splash screen"""
     print(clr("* NET TOOLS *", "black", "on_white", ["bold"]))
-    print(f"Version 0.0.2 {clr("alpha","green")}")
+    print(f"Version {VERSION_NUMBER} {VERSION_NAME}")
     print("")
     print("MIT license")
     print("Copyright(c) Nícolas Sousa, 2024")
-    print("\n")  # 3 empty lines
-    print(f"Type {clr("'help'","green" )} to see available commands\n")
+    print("\n")
+    print(f"Type {clr('`help`','green' )} to see available commands\n")
     print("")
 
-def display_user_prompt(_app_name:Optional[str]="") -> str:
+
+def display_user_prompt(_app_name: Optional[str] = "") -> str:
     """
     Displays a writing prompt for the user
-    
+
     returns:
         str: The raw string the user typed in
     """
     username = getpass.getuser()
     domain = os.getenv("USERDOMAIN")
     # In case of no domain assume we are at home
-    if isinstance(domain,type(None)):
+    if isinstance(domain, type(None)):
         domain = "Home"
     print(
-        f"\n{clr(username, "blue", attrs=["bold"])}"
+        f"\n{clr(username, 'blue', attrs=['bold'])}"
         " @ "
-        f"{clr(domain,"magenta",attrs=["bold"])}"
+        f"{clr(domain,'magenta',attrs=['bold'])}"
     )
-    print(_app_name, "> ",end="")
+    print(_app_name, "> ", end="")
 
     user_input = input()
     print("")
@@ -41,10 +46,17 @@ def display_user_prompt(_app_name:Optional[str]="") -> str:
     return user_input
 
 
-
-# Show main menu
-
-
 # Show help menu
-def display_help():
-    ...
+def display_help(arguments: list[str]):
+    mprint(
+        "",
+        f"Net-tools {VERSION_NUMBER} {VERSION_NAME}",
+        "",
+        f"{clr('Commands:',attrs=['bold'])}",
+        "`chip`    Check IP",
+        "`chil`    Check IP list",
+        "`help`    Help menu",
+        "`exit`    Exits the program",
+        "",
+        "`Use `help [program name]` to learn specifics",
+    )
